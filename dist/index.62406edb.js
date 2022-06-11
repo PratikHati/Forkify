97c3cb50a97297f64a927f02990b05aad109f2f9
Pratik Hati <pratikhati.work@gmail.com>
2022-06-11 21:27:17 +0530

    First

---
+++ dist/index.62406edb.js
@@ -0,0 +1,38 @@
+const recipeContainer = document.querySelector(".recipe");
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        //async is used to create a new thread with out affecting original application thread
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"); //ajax call
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.Error);
+        else {
+            console.log(res, data);
+            let { recipe  } = data.data;
+            recipe = {
+                id: recipe.id,
+                title: recipe.title,
+                publisher: recipe.publisher,
+                sourceUrl: recipe.source_url,
+                image: recipe.image_url,
+                servings: recipe.servings,
+                cookingTime: recipe.cooking_time,
+                ingredients: recipe.ingredients
+            };
+            console.log(recipe);
+        }
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
